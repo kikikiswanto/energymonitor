23a9037ebd7d896f2adebb28d0fc1c26de1157a0
kiki kiswanto <kiki.kiswanto@hellobisnis.com>
2017-01-14 09:13:38 +0700

    modify dinamic config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session','encryption_kay');
+$autoload['libraries'] = array('session','database','encryption_kay');
 
 /*
 | -------------------------------------------------------------------
